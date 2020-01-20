41024ef5beedac00980c6d354c6228dca1e52b7f
Shasaeb <43362136+Shasaeb@users.noreply.github.com>
2020-01-20 05:37:54 -0700

    email update

--- contact/handler.php
+++ contact/handler.php
@@ -21,6 +21,6 @@ $validator->field('comments')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('admin@kristopherlafrance.tech'); // ← Your email here
+$pp->sendEmailTo('krislafrance0@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
